7b0f5eeaba357540116af9ba388cb8edc9cf0713
Shoptorshi Jamil <sjami002@gmail.com>
2019-04-11 23:50:56 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -4,4 +4,14 @@ class CreateArtists < ActiveRecord::Migration
  
   def down
   end
+  
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+  
 end
